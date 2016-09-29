0d2beb5d8d64d0544997291c619293c027691788
Emil Falk <emfa@riseup.net>
2016-09-29 21:15:44 +0200

    Use varint from the beginning so we wont have to change it in the future.

--- src/multibase.erl
+++ src/multibase.erl
@@ -15,50 +15,68 @@
 -define(BASE64, $y).
 -define(BASE64URL, $Y).
 
--export([encode/2, decode/1]).
+-type base() :: base1 | base2 | base8 | base10 | base16
+              | base32 | base32hex
+              | base58flickr | base58btc
+              | base64 | base64url.
 
+-type multibase() :: unicode:chardata().
+
+-export_type([base/0, multibase/0]).
+
+-export([encode/1, encode/2, decode/1]).
+
+-spec encode(binary()) -> multibase().
+encode(Bin) ->
+    encode(base64, Bin).
+
+-spec encode(base(), binary()) -> multibase().
 encode(base1, Bin) ->
-    <<?BASE1, (encode_base_1(Bin))/binary>>;
+    [varint:encode(?BASE1), encode_base_1(Bin)];
 encode(base2, Bin) ->
-    <<?BASE2, (integer_to_binary(binary:decode_unsigned(Bin), 2))/binary>>;
+    [varint:encode(?BASE2), integer_to_binary(binary:decode_unsigned(Bin), 2)];
 encode(base8, Bin) ->
-    <<?BASE8, (integer_to_binary(binary:decode_unsigned(Bin), 8))/binary>>;
+    [varint:encode(?BASE8), integer_to_binary(binary:decode_unsigned(Bin), 8)];
 encode(base10, Bin) ->
-    <<?BASE10, (integer_to_binary(binary:decode_unsigned(Bin), 10))/binary>>;
+   [varint:encode(?BASE10), integer_to_binary(binary:decode_unsigned(Bin), 10)];
 encode(base16, Bin) ->
-    <<?BASE16, (integer_to_binary(binary:decode_unsigned(Bin), 16))/binary>>;
+    [varint:encode(?BASE16), integer_to_binary(binary:decode_unsigned(Bin), 16)];
 encode(base32, Bin) ->
-    <<?BASE32, (base32:encode(Bin))/binary>>;
+    [varint:encode(?BASE32), base32:encode(Bin)];
 encode(base32hex, Bin) ->
-    <<?BASE32HEX, (base32:encode(Bin, [hex]))/binary>>;
+    [varint:encode(?BASE32HEX), base32:encode(Bin, [hex])];
 encode(base58flickr, _Bin) ->
     {error, unimplemented};
 encode(base58btc, _Bin) ->
     {error, unimplemented};
-encode({base, 64}, Bin) ->
-    <<?BASE64, (base64:encode(Bin))/binary>>;
+encode(base64, Bin) ->
+    [varint:encode(?BASE64), base64:encode(Bin)];
 encode(base64url, Bin) ->
-    <<?BASE64URL, (base64url:encode(Bin))/binary>>.
+    [varint:encode(?BASE64URL), base64url:encode(Bin)].
+
+decode(MB0) ->
+    {Base, MB1} = varint:decode(MB0),
+    decode(Base, MB1).
                              
-decode(<<?BASE1, Rest/binary>>) ->
+decode(?BASE1, Rest) ->
     binary:encode_unsigned(decode_base_1(Rest));
-decode(<<?BASE2, Rest/binary>>) ->
+decode(?BASE2, Rest) ->
     binary:encode_unsigned(binary_to_integer(Rest, 2));
-decode(<<?BASE8, Rest/binary>>) ->
+decode(?BASE8, Rest) ->
     binary:encode_unsigned(binary_to_integer(Rest, 8));
-decode(<<?BASE10, Rest/binary>>) ->
+decode(?BASE10, Rest) ->
     binary:encode_unsigned(binary_to_integer(Rest, 10));
-decode(<<Base, Rest/binary>>) when Base == ?BASE16; Base == ?base16 ->
+decode(Base, Rest) when Base == ?BASE16; Base == ?base16 ->
     binary:encode_unsigned(binary_to_integer(Rest, 16));
-decode(<<Base, Rest/binary>>) when Base == ?BASE32; Base == ?base32 ->
+decode(Base, Rest) when Base == ?BASE32; Base == ?base32 ->
     binary:encode_unsigned(binary_to_integer(Rest, 32));
-decode(<<?BASE58FLICKR, _Rest/binary>>) ->
+decode(?BASE58FLICKR, _Rest) ->
     {error, unimplemented};
-decode(<<?BASE58BTC, _Rest/binary>>) ->
+decode(?BASE58BTC, _Rest) ->
     {error, unimplemented};
-decode(<<?BASE64, Rest/binary>>) ->
+decode(?BASE64, Rest) ->
     base64:decode(Rest);
-decode(<<?BASE64URL, Rest/binary>>) ->
+decode(?BASE64URL, Rest) ->
     base64url:decode(Rest).
 
 encode_base_1(Bin) ->
@@ -76,4 +94,3 @@ decode_base_1(<<>>, Ones) ->
     Ones;
 decode_base_1(<<$1, Rest/binary>>, Ones) ->
     decode_base_1(Rest, Ones + 1).
-
